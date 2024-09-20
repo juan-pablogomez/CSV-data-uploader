0b7ef4ad58613500326e361e0ef744029ecd1356
juan-pablogomez <juan-pablogomez@hotmail.com>
2024-09-20 16:50:45 -0500

    Initial commit

---
+++ csv-data-uploader.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * 
+ * Plugin Name: CSV data uploader
+ * Plugin URI: https://github.com/juan-pablogomez
+ * Description: Plugin for upload csv Data, 
+ * Version: 1.0
+ * Requires at least: 5.8
+ * Requires PHP: 7.4
+ * Author: Juan Pablo Gómez
+ * Author URI: https://curriculum-pablo.netlify.app/
+ * License: GPL v2 or later  
+ * @package csvplugin
+ *  
+ */
+
